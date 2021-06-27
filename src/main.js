9ab0b446b8ce73f863c3d5fde398eaacff6acfdf
Sebastian Benitez <jsb1439@hotmail.com>
2021-06-27 00:17:58 -0500

    Challenge solution

--- src/main.js
+++ src/main.js
@@ -1,13 +1,52 @@
 import { getPokemonList } from './api/index.js';
 import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
-const main = async () => {
-	const app = document.getElementById("app");
-	const dropdownTypes = document.getElementById("dropdown-types");
-	const pokemonList = await getPokemonList();
+const addListeners = (domElements) => {
+	const {
+		mainContent,
+		headerCheckbox,
+		dropdownTypes,
+		cardListItems
+	} = domElements;
+
+	headerCheckbox.addEventListener('change', () => {
+		mainContent.classList.toggle('dark', headerCheckbox.checked);
+	});
+
+	dropdownTypes.addEventListener('change', () => {
+		const selectedValue = dropdownTypes.value;
+
+		cardListItems.forEach(item => {
+			if (selectedValue === 'all') {
+				item.classList.remove('hide');
+			} else {
+				const itemTypes = item.getAttribute('data-types');
+				const hasType = itemTypes.includes(selectedValue);
 	
+				item.classList.toggle('hide', !hasType);
+			}
+		});
+	});	
+};
+
+const main = async () => {
+	const pokemonList = await getPokemonList();
+	const domElements = {
+		root: document.getElementById("app"),
+		dropdownTypes: document.getElementById("dropdown-types"),
+		headerCheckbox: document.getElementById('header-checkbox'),
+		mainContent: document.querySelector('.main-content')
+	};
+
+	// Populate DOM
 	createPokemonList(pokemonList, app);
-	createDropdownTypes(pokemonList, dropdownTypes);
+	createDropdownTypes(pokemonList, domElements.dropdownTypes);
+
+	// Get Card List
+	domElements.cardListItems = document.querySelectorAll('.card-list__item');
+
+	// Add Listeners
+	addListeners(domElements);
 };
 
 main();
